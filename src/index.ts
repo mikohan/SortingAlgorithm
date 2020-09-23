4af906d5b0f5cf285546cb60a2b9b6d05eba6d90
mikohan <angara99@gmail.com>
2020-09-23 17:25:49 +0300

    Implemented linkedList class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // import { NumberCollection } from './NumbersCollection';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 import { Sorter } from './Sorter';
 
 // const numbersCollection = new NumberCollection([10, 3, -5, 0]);
@@ -11,8 +12,17 @@ import { Sorter } from './Sorter';
 // sorter.sort();
 // console.log('Sorted collection', numberCollection.data);
 
-const charactersCollection = new CharactersCollection('some cool string');
+// const charactersCollection = new CharactersCollection('some cool string');
 
-const sorter = new Sorter(charactersCollection);
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
